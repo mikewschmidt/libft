60fa0f6ddbb95610e23f3af0242738e15a2d9f28
Mike Schmidt <michaelwschmidt@gmail.com>
2020-05-28 05:13:05 -0700

    Update ft_putendl_fd.c
    used ft_putstr_fd and removed the loop

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -14,15 +14,8 @@
 
 void	ft_putendl_fd(char const *s, int fd)
 {
-	int		i;
-
-	i = 0;
 	if (!s || !fd)
 		return ;
-	while (s[i] != '\0')
-	{
-		write(fd, &s[i], 1);
-		i++;
-	}
+	ft_putstr_fd(s, fd);
 	write(fd, "\n", 1);
 }
